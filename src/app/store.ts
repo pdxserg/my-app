fa0d99f88f0a5846c8541677f088d93c29ae9e8e
SergeyStarchenko <pdxserg@gmail.com>
2024-11-26 21:57:20 -0800

    refactoring

---
+++ src/app/store.ts
@@ -0,0 +1,27 @@
+import {  combineReducers, UnknownAction } from "redux"
+import {  ThunkDispatch } from "redux-thunk"
+import { authReducer } from "../features/auth/model/authSlice"
+import { tasksReducer } from "../features/todolists/model/tasks-reducer"
+import { todolistsReducer } from "../features/todolists/model/todolists-reducer"
+import { appReducer } from "./appSlice"
+import {configureStore} from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+  tasks: tasksReducer,
+  todolists: todolistsReducer,
+  app: appReducer,
+  auth: authReducer,
+})
+
+//export const store = legacy_createStore(rootReducer, {}, applyMiddleware(thunk))
+export const store = configureStore({ reducer: rootReducer })
+
+export type RootState = ReturnType<typeof store.getState>
+
+// export type AppDispatch = typeof store.dispatch
+
+// Создаем тип диспатча который принимает как AC так и TC
+export type AppDispatch = ThunkDispatch<RootState, unknown, UnknownAction>
+
+// @ts-ignore
+window.store = store
---
+++ src/features/auth/model/authSlice.ts
@@ -0,0 +1,89 @@
+import {ResultCode} from "common/enums"
+import {handleServerAppError} from "common/utils/handleServerAppError"
+import {handleServerNetworkError} from "common/utils/handleServerNetworkError"
+import {Dispatch} from "redux"
+import {setAppStatusAC} from "../../../app/appSlice"
+import {clearTasksAC} from "../../todolists/model/tasks-reducer"
+import {clearTodolistsAC} from "../../todolists/model/todolists-reducer"
+import {authApi} from "../api/authAPI"
+import {LoginArgs} from "../api/authAPI.types"
+import {createSlice} from "@reduxjs/toolkit";
+
+
+const authSlice = createSlice({
+	name: 'auth',
+	initialState: {
+		isLoggedIn: false,
+		isInitialized: false,
+	},
+  reducers: create => ({
+    setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
+      state.isLoggedIn = action.payload.isLoggedIn
+    }),
+    setIsInitialized: create.reducer<{ isInitialized: boolean }>((state, action) => {
+      state.isInitialized = action.payload.isInitialized
+    }),
+  }),
+})
+
+export const {setIsLoggedIn, setIsInitialized} = authSlice.actions
+export const authReducer = authSlice.reducer
+
+// thunks
+export const loginTC = (data: LoginArgs) => (dispatch: Dispatch) => {
+	dispatch(setAppStatusAC("loading"))
+	authApi
+		.login(data)
+		.then((res) => {
+			if (res.data.resultCode === ResultCode.Success) {
+				dispatch(setAppStatusAC("succeeded"))
+				dispatch(setIsLoggedIn({isLoggedIn: true}))
+				localStorage.setItem("sn-token", res.data.data.token)
+			} else {
+				handleServerAppError(res.data, dispatch)
+			}
+		})
+		.catch((error) => {
+			handleServerNetworkError(error, dispatch)
+		})
+}
+
+export const logoutTC = () => (dispatch: Dispatch) => {
+	dispatch(setAppStatusAC("loading"))
+	authApi
+		.logout()
+		.then((res) => {
+			if (res.data.resultCode === ResultCode.Success) {
+				dispatch(setAppStatusAC("succeeded"))
+				dispatch(setIsLoggedIn({isLoggedIn: false}))
+				dispatch(clearTasksAC())
+				dispatch(clearTodolistsAC())
+				localStorage.removeItem("sn-token")
+			} else {
+				handleServerAppError(res.data, dispatch)
+			}
+		})
+		.catch((error) => {
+			handleServerNetworkError(error, dispatch)
+		})
+}
+
+export const initializeAppTC = () => (dispatch: Dispatch) => {
+	dispatch(setAppStatusAC("loading"))
+	authApi
+		.me()
+		.then((res) => {
+			if (res.data.resultCode === ResultCode.Success) {
+				dispatch(setAppStatusAC("succeeded"))
+				dispatch(setIsLoggedIn({isLoggedIn: true}))
+			} else {
+				handleServerAppError(res.data, dispatch)
+			}
+		})
+		.catch((error) => {
+			handleServerNetworkError(error, dispatch)
+		})
+		.finally(() => {
+			dispatch(setIsInitialized({isInitialized: true}))
+		})
+}
