b9aca18154dece21a67b349c29e7fd185b43506e
SergeyStarchenko <pdxserg@gmail.com>
2024-11-28 11:38:19 -0800

    refactoring

--- src/app/store.ts
+++ src/app/store.ts
@@ -6,16 +6,22 @@ import { todolistsReducer } from "../features/todolists/model/todolistsSlice"
 import { appReducer } from "./appSlice"
 import {configureStore} from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-  auth: authReducer,
-})
+// const rootReducer = combineReducers({
+//   tasks: tasksReducer,
+//   todolists: todolistsReducer,
+//   app: appReducer,
+//   auth: authReducer,
+// })
 
-//export const store = legacy_createStore(rootReducer, {}, applyMiddleware(thunk))
-export const store = configureStore({ reducer: rootReducer })
-
+//1 export const store = legacy_createStore(rootReducer, {}, applyMiddleware(thunk))
+//2 export const store = configureStore({ reducer: rootReducer })
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer,
+  } })
 export type RootState = ReturnType<typeof store.getState>
 
 // export type AppDispatch = typeof store.dispatch
@@ -23,5 +29,4 @@ export type RootState = ReturnType<typeof store.getState>
 // Создаем тип диспатча который принимает как AC так и TC
 export type AppDispatch = ThunkDispatch<RootState, unknown, UnknownAction>
 
-// @ts-ignore
-window.store = store
+
